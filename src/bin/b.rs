2c5d3c23d41e07845d9e7290e51e87c43b172071
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-19 21:29:32 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    unimplemented!();
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).ok().unwrap();
+
+    let _n = buf.trim()
+                .to_string()
+                .parse::<i32>()
+                .expect("parse error 1");
+
+    buf.clear();
+    io::stdin().read_line(&mut buf).ok().unwrap();
+
+    let mut left: i32 = 0;
+    let (ans, _) = buf.split_whitespace()
+                      .map(|s| String::from(s).parse::<i32>().expect("parse error 2"))
+                      .map(|num| cmp_substitute(&mut left, num))
+                      .take_while(|&(_, right)| right)
+                      .last()
+                      .unwrap();
+
+    println!("{}", ans);
+}
+
+fn cmp_substitute(left: &mut i32, num: i32) -> (i32, bool) {
+    let ans = *left < num;
+    *left = num;
+    (num, ans)
 }
