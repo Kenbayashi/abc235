40c1151dbd7783ebd5a7265e22c04b442931128f
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-19 21:00:59 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,11 @@
+use std::io;
+use std::io::Read;
+
 fn main() {
-    unimplemented!();
+    let sum: i32 = io::stdin().bytes()
+                         .map(|c| String::from(c.unwrap() as char))
+                         .map(|s| s.parse::<i32>().ok().unwrap())
+                         .sum();
+
+    println!("{}", 100 * sum + 10 * sum + sum);
 }
